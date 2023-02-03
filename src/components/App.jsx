117115630abd2ff67168cc55babe4b646ebb3437
Anton Shabalov <shabalovanton@gmail.com>
2023-02-03 23:27:58 +0200

    fix state in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,50 @@
 import { Component } from "react";
 import { Toaster } from "react-hot-toast";
+import { Loader } from "components/Loader/Loader"
+import { Button } from "components/Button/Button"
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Modal } from "./Modal/Modal";
 import { Searchbar } from "./Searchbar/Searchbar"
+import { fetchImgs } from "js/fetchSearch"
+import toast from 'react-hot-toast';
 
 export class App extends Component {
   state = {
     tagImg: null,
+    imgs: null,
+    responseApi: null,
+    status: 'idel',
+    page: 2,
+    error: null,
     showModal: false,
     showImg: null
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.tagImg !== this.state.tagImg) {
+      this.setState({page: 2, status: 'pending'})
+      fetchImgs(this.state.tagImg, 1)
+      .then(imgs => this.setState(prevState => {
+        if (!imgs.total) {
+          return {imgs: null, status: 'absent'}
+        }
+        if (Math.ceil(imgs.totalHits / 12) === 1) {
+          return {imgs: imgs.hits, status: 'end'}
+        }
+        return {imgs: imgs.hits, responseApi: imgs, status: 'resolved'}}))
+      .catch(error => this.setState({error, status: 'rejected'}))
+    }
+  }
+
+  loadMore = () => {
+    this.setState(({page}) => {return {page: page+1, status: 'pending'}})
+    fetchImgs(this.state.tagImg, this.state.page)
+      .then(imgs => this.setState(prevState => {
+        if (Math.ceil(this.state.responseApi.totalHits / 8) === this.state.page) {
+          return {imgs: [...prevState.imgs, ...imgs.hits], status: 'end'}
+        }
+        return {imgs: [...prevState.imgs, ...imgs.hits], status: 'resolved'}
+      }))
   }
 
   toggleModal = () => {
@@ -31,11 +67,19 @@ export class App extends Component {
 
   render () {
 
+    const {imgs, showModal, status, tagImg, showImg} = this.state
+
     return (
       <>
         <Searchbar onSubmit = {this.handelFormSubmit}/>
-        <ImageGallery tagImg ={this.state.tagImg} onOpenModal={this.openModal}/>
-        {this.state.showModal && <Modal img = {this.state.showImg} onClose = {this.closeModal}/>}
+        {imgs && <ImageGallery imgs ={imgs} onOpenModal={this.openModal}/>}
+        {showModal && <Modal img = {showImg} onClose = {this.closeModal}/>}
+        {status === 'pending' && <Loader />}
+        {status === 'end' && <h3>Це всі зображення, що знайшли 🙃</h3>}
+        {status === 'resolved' && <Button onClick={this.loadMore}/>}
+        {status === 'absent' && <h3>No results found for {tagImg}.</h3>}
+        {status === 'rejected' && <h3>ERROR</h3>}
+        
         <Toaster position="bottom-center"/>
       </>
     );
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,83 +1,21 @@
 
-import { Button } from "components/Button/Button"
-import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
-import { Loader } from "components/Loader/Loader"
 import { Component } from "react"
-import { fetchImgs } from "js/fetchSearch"
+import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
+
 import { ImageGalleryEl } from './ImageGallery.styled'
 
+import { Loader } from "components/Loader/Loader"
 
-export class ImageGallery extends Component {
-  state = {
-    imgs: null,
-    status: 'idel',
-    page: 2,
-    error: null
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.tagImg !== this.props.tagImg) {
-      const firstPage = 1
-      this.setState({page: 2})
-      this.setState({status: 'pending'})
-      fetchImgs(this.props.tagImg, firstPage)
-      .then(imgs => {
-        if (!imgs.total) {
-          return this.setState({status: 'absent'})
-        }
-        return this.setState({imgs: imgs.hits, status: 'resolved'})})
-      .catch(error => this.setState({error, status: 'rejected'}))
-    }
-  }
+export const ImageGallery = ({imgs, onOpenModal}) => {
 
-  loadMore = () => {
-    this.setState(({page}) => {return {page: page+1, status: 'pendingMore'}})
-    fetchImgs(this.props.tagImg, this.state.page)
-      .then(imgs => this.setState(prevState => {
-        return {imgs: [...prevState.imgs, ...imgs.hits], status: 'resolved'}
-      }))
-  }
-
-  render () {
-
-    if (this.state.status === 'pending') {
-      return <Loader />
-    }
-    
-    if (this.state.status === 'resolved') {
-      return (
-        <>
-          <ImageGalleryEl>
-            {this.state.imgs.map(img => <ImageGalleryItem img={img} onClick={this.props.onOpenModal}  key={img.id}/>)}
-          </ImageGalleryEl>
-          {false 
-            ? <h3>Це всі зображення 🙃</h3> 
-            : <Button onClick={this.loadMore}/>}
-          
-        </>
-      )
-    }
-
-    if (this.state.status === 'pendingMore') {
-      return (
-        <>
-          <ImageGalleryEl>
-            {this.state.imgs.map(img => <ImageGalleryItem img={img}  key={img.id}/>)}
-          </ImageGalleryEl>
-          <Loader />
-        </>
-      )
-    }
-
-    if (this.state.status === 'absent') {
-      return <p>No results found for {this.props.tagImg}.</p>
-    }
-
-    if (this.state.status === 'rejected') {
-      console.dir(this.state.error)
-      return <h3>ERROR</h3>
-    }
-    
-  }
-  
+  return (
+    <>
+      <ImageGalleryEl>
+        {imgs.map(img => <ImageGalleryItem img={img} onClick={onOpenModal}  key={img.id}/>)}
+      </ImageGalleryEl>
+    </>
+  )
 }
+
+// 'resolved' 'pendingMore' 'idel' 'pendingMore' 'end'
